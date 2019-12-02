819e61bd58fd4dd77a3fce7ed1a80581c62c55f2
Troy Kelly <troy@troykelly.com>
2019-12-02 18:03:33 +1100

    Unimus v0.0.9

--- unimus/data/run.sh
+++ unimus/data/run.sh
@@ -12,8 +12,19 @@ DATABASE_NAME=$(bashio::config 'database.name')
 DATABASE_USER=$(bashio::config 'database.user')
 DATABASE_PASSWORD=$(bashio::config 'database.password')
 LOGGING=$(bashio::config 'logging')
+SSLCERTFILE=$(bashio::config 'certfile')
+SSLKEYFILE=$(bashio::config 'keyfile')
 
 ## Main ##
+if bashio::config.true 'ssl'; then
+  bashio::log.info "Generating SSL p12 for Tomcat"
+  SSLPASSWORD=$(date | md5)
+  rm -Rf /usr/src/app/Cert.p12
+  openssl pkcs12 -export -out /usr/src/app/unimus.keystore.p12 -in /ssl/"$SSLCERTFILE" -inkey /ssl/"$SSLKEYFILE" -name unimus -passout pass:"$SSLPASSWORD"
+  echo "-Dserver.ssl.enabled=true -Dserver.ssl.key-store=/usr/src/app/unimus.keystore.p12 -Dserver.ssl.keyStoreType=PKCS12 -Dserver.ssl.keyAlias=unimus -Dserver.ssl.key-store-password=$SSLPASSWORD -Dserver.port=8099" > /etc/default/unimus
+else
+  echo "-Dserver.port=8099" > /etc/default/unimus
+fi
 
 bashio::log.info "Setup Unimus configuration"
 sed -E -i "s/^(#.*)*(license\.key) *= *(.*)$/\2 = $LICENSE_KEY/g" /etc/unimus/unimus.properties
@@ -25,14 +36,15 @@ sed -E -i "s/^(#.*)*(database\.user) *= *(.*)$/\2 = $DATABASE_USER/g" /etc/unimu
 sed -E -i "s/^(#.*)*(database\.password) *= *(.*)$/\2 = $DATABASE_PASSWORD/g" /etc/unimus/unimus.properties
 
 # Start Unimus Server
-java -jar /usr/src/app/Unimus.jar &
+bashio::log.info "Starting Unimus system"
+java $(/bin/cat /etc/default/unimus 2> /dev/null) -jar /usr/src/app/Unimus.jar &
 WAIT_PIDS+=($!)
 
 # Handling Closing
 function stop_unimus() {
     bashio::log.info "Shutdown Unimus system"
     kill -15 "${WAIT_PIDS[@]}"
-    
+
     wait "${WAIT_PIDS[@]}"
 }
 trap "stop_unimus" SIGTERM SIGHUP
