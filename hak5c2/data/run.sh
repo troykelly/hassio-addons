0866e3578f7155bf60e3878cb1cc280e0cbe9f71
Troy Kelly <troy@troykelly.com>
2020-01-15 08:09:19 +1100

    Allowing other arch in hak5c2

--- hak5c2/data/run.sh
+++ hak5c2/data/run.sh
@@ -5,19 +5,31 @@ LOGGING=$(bashio::config 'logging')
 SSLCERTFILE=$(bashio::config 'certfile')
 SSLKEYFILE=$(bashio::config 'keyfile')
 C2HOSTNAME=$(bashio::config 'hostname')
+HOST_ARCH=$(bashio::homeassistant.arch)
 C2DB="/data/c2"
 
 mkdir -p "${C2DB}"
+
+C2_ARCH="linux-64"
+if [ "$HOST_ARCH" == "i386" ]; then
+  C2_ARCH="linux-32"
+fi
+if [ "$HOST_ARCH" == "armv7" ]; then
+  C2_ARCH="linux-armv7"
+fi
+if [ "$HOST_ARCH" == "armhf" ]; then
+  C2_ARCH="linux-armv7"
+fi
 
 ## Main ##
 if bashio::config.true 'ssl'; then
   SSLPASSWORD=$(date | md5)
   bashio::log.info "Starting Hak5 C2 with SSL on ${C2HOSTNAME}"
-  /usr/src/app/c2_community-linux-64 -db "${C2DB}"/c2.db -certFile /ssl/"$SSLCERTFILE" -keyFile /ssl/"$SSLKEYFILE" -https -hostname "${C2HOSTNAME}" -reverseProxy -reverseProxyPort 443 -listenport 8686 &
+  /usr/src/app/c2_community-${C2_ARCH} -db "${C2DB}"/c2.db -certFile /ssl/"$SSLCERTFILE" -keyFile /ssl/"$SSLKEYFILE" -https -hostname "${C2HOSTNAME}" -reverseProxy -reverseProxyPort 443 -listenport 8686 &
   WAIT_PIDS+=($!)
 else
   bashio::log.info "Starting Hak5 C2 on ${C2HOSTNAME}"
-  /usr/src/app/c2_community-linux-64 -db "${C2DB}"/c2.db -hostname "${C2HOSTNAME}" -reverseProxy -reverseProxyPort 80 -listenport 8686 &
+  /usr/src/app/c2_community-${C2_ARCH} -db "${C2DB}"/c2.db -hostname "${C2HOSTNAME}" -reverseProxy -reverseProxyPort 80 -listenport 8686 &
   WAIT_PIDS+=($!)
 fi
 
@@ -31,8 +43,6 @@ function stop_hak5c2() {
 trap "stop_hak5c2" SIGTERM SIGHUP
 
 bashio::log.info "C2 ready on ${C2HOSTNAME}"
-sleep 20
-netstat -an
 
 # Wait and hold Add-on running
 wait "${WAIT_PIDS[@]}"
