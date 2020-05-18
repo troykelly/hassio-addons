a2fe9efdb420706ae2aeb2d60f0177934247698f
Troy Kelly <troy@troykelly.com>
2020-05-19 08:39:10 +1000

    Unimus version 2.0.0

--- _tools/build.sh
+++ _tools/build.sh
@@ -1,12 +1,28 @@
 #!/usr/bin/env bash
 
-uniums=$(cd ./unimus && pwd)
 dahuavtomqtt=$(cd ./DahuaVTO2MQTT && pwd)
+hak5c2=$(cd ./hak5c2 && pwd)
+letslexicon=$(cd ./letslexicon && pwd)
+uniums=$(cd ./unimus && pwd)
 
 echo "Building for ${dahuavtomqtt} uploading to Docker Hub as ${DOCKER_USER}"
 
 docker run --rm --privileged \
     -v ${dahuavtomqtt}:/data \
+    homeassistant/amd64-builder --docker-user "${DOCKER_USER}" --docker-password "${DOCKER_PASSWORD}" \
+    --all -t /data
+
+echo "Building for ${hak5c2} uploading to Docker Hub as ${DOCKER_USER}"
+
+docker run --rm --privileged \
+    -v ${hak5c2}:/data \
+    homeassistant/amd64-builder --docker-user "${DOCKER_USER}" --docker-password "${DOCKER_PASSWORD}" \
+    --all -t /data
+
+echo "Building for ${letslexicon} uploading to Docker Hub as ${DOCKER_USER}"
+
+docker run --rm --privileged \
+    -v ${letslexicon}:/data \
     homeassistant/amd64-builder --docker-user "${DOCKER_USER}" --docker-password "${DOCKER_PASSWORD}" \
     --all -t /data
 
